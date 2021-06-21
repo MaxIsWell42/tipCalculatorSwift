a08e2434434c8e85a5b524f60b79df676e389f9e
HexSeal <maxfin13@gmail.com>
2021-06-21 11:34:00 -0400

    [init], [final]

---
+++ TipCalculatorStarter/ColorTheme.swift
@@ -0,0 +1,42 @@
+//
+//  ColorTheme.swift
+//  TipCalculatorStarter
+//
+//  Created by Maxwell Finn on 6/21/21.
+//  Copyright © 2021 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColorTheme {
+    // MARK: - Class Vars
+    static let light = ColorTheme(isDefaultStatusBar: true,
+                                  viewControllerBackgroundColor: .tcOffWhite,
+                                  primaryColor: .tcWhite,
+                                  primaryTextColor: .tcCharcoal,
+                                  secondaryColor: .tcDarkBlue,
+                                  accentColor: .tcHotPink,
+                                  outputTextColor: .tcAlmostBlack)
+
+    static let dark = ColorTheme(isDefaultStatusBar: false,
+                                 viewControllerBackgroundColor: .tcAlmostBlack,
+                                 primaryColor: .tcMediumBlack,
+                                 primaryTextColor: .tcWhite,
+                                 secondaryColor: .tcBlueBlack,
+                                 accentColor: .tcSeafoamGreen,
+                                 outputTextColor: .tcWhite)
+    
+    // MARK: - Instance Vars
+
+    let isDefaultStatusBar: Bool
+    let viewControllerBackgroundColor: UIColor
+
+    let primaryColor: UIColor
+    let primaryTextColor: UIColor
+
+    let secondaryColor: UIColor
+
+    let accentColor: UIColor
+    let outputTextColor: UIColor
+}
